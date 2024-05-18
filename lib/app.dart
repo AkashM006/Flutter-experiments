dd277074182078dc8b29339085a8c2b7a6db066c
Akash M <akashm006@outlook.com>
2024-05-18 14:52:02 +0530

    Formatter logic fixed

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:sample/formatter.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text("Welcome"),
+        child: TextField(
+          keyboardType: TextInputType.number,
+          inputFormatters: [IntegerFormatter()],
+        ),
       ),
     );
   }
